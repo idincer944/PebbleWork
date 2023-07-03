e6a6ec7f5ca167d8e391a17021fddf2f7070120a
İsmail <idincer944@gmail.com>
2023-07-03 23:53:31 +0300

    User schema implemented

---
+++ src/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstname: {
+    type: String,
+  },
+  lastname: {
+    type: String,
+  },
+  password_hash: {
+    type: String,
+    required: true,
+  },
+  registered_at: {
+    type: Date,
+    default: Date.now,
+  },
+  avatar: {
+    type: String,
+  },
+  created_events: {
+    type: Array,
+  },
+  joined_events: {
+    type: Array,
+  },
+});
+
+userSchema.pre('validate', function (next) {
+  // slugify username
+  this.username = slugify(
+    this.username.trim().replace(/[^a-zA-Z0-9 .]/g, '.'),
+    {
+      replacement: '.',
+      lower: 'true',
+      strict: 'true',
+    }
+  );
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
