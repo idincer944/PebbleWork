3221783033ffb45224677906cacce9e81a3fd70d
Ilayda Nur Guzel <122798694+ilaydanurguzel1@users.noreply.github.com>
2023-07-14 15:34:29 +0300

    middleware-auth.js (#27)

--- src/app.js
+++ src/app.js
@@ -15,12 +15,13 @@ connectToMongo();
 
 const PORT = process.env.NODE_LOCAL_PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 
