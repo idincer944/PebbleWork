bd21c8f481b29be31f36551fea0dd0ca819c80ab
Ilayda Nur Guzel <ilaydanurguzel1@gmail.com>
2023-07-14 14:29:21 +0300

    middleware-auth.js

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 const authenticate = () => {
   return (req, res, next) => {
@@ -10,7 +11,20 @@ const authenticate = () => {
           return res.status(403).json({ error: 'Invalid token' });
         }
         req.user = user;
-        next();
+
+        //Checking if the user is an admin
+        User.findByPk(user.userId).then(user => {
+          user.getRoles().then(roles => {
+            for (let i=0; i < roles.length; i++){
+              if (roles[i].name === 'admin') {
+                next();
+                  return;
+              }
+            }
+            res.status(403).send({ message: 'Require Admin Role !'})
+          });
+        });
+
       });
     } else {
       return res.status(401).json({ error: 'Unauthorized' })
