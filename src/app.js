ce8e7a7ad7298d75ad8d0c45786ba50a71e8e755
Ilayda Nur Guzel <ilaydanurguzel1@gmail.com>
2023-07-24 16:42:40 +0300

    adding swagger documentation for user

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const userRouter = require('./routes/user');
 const eventRouter = require('./routes/event');
 const connectToMongo = require('./db');
+const fs = require('fs');
+const path = require('path');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const { swaggerDocument } = require('./server/swagger'); // Importing the Swagger file
 
 const app = express();
 
@@ -20,6 +26,40 @@ const PORT = process.env.NODE_LOCAL_PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+let swaggerSpec = {
+  openapi: '3.1.0',
+  info: {
+    title: 'Express API with Swagger',
+    version: '1.0.0',
+    description: 'This is a simple API application made with Express and documented with Swagger',
+    license: { name: 'MIT', url: 'https://spdx.org/licenses/MIT.html' }
+  },
+  servers: [{ url: 'http://localhost:3000/api-docs' }],
+  paths: {},
+  components: {},
+  tags: [],
+  apis: ["./routes/user.js"],
+};
+
+const filePath = path.join(__dirname, './server/swagger.json');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the Swagger JSON file:', err);
+    return;
+  }
+  try {
+    const swaggerDocument = JSON.parse(data);
+
+    Object.assign(swaggerSpec, swaggerDocument);
+
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
+
+  } catch (parseError) {
+    console.error('Error parsing the Swagger JSON data:', parseError);
+  }
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
