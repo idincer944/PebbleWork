0c59e12214c4cffd9a5cce7ad799f0ae9b17858f
said baradai <saitbaradai@gmail.com>
2023-07-31 17:49:52 +0300

    fix : routes pretifiying

--- src/routes/event.js
+++ src/routes/event.js
@@ -10,43 +10,20 @@ router.get('/getAllEvents', eventController.getAllEvents);
 router.get('/createdEvents', authenticate(), eventController.getAllUserEvents);
 router.post('/createNewEvent', authenticate(), eventController.createNewEvent);
 router.get('/getEventById/:eventId', eventController.getEventById);
-router.delete(
-  '/deleteEvent/:eventId',
-  authenticate(),
-  eventController.cancleEvent
-);
-router.put(
-  '/updateEvent/:eventId',
-  authenticate(),
-  eventController.updateEvent
-);
+router.delete('/deleteEvent/:eventId',authenticate(),eventController.cancleEvent);
+router.put('/updateEvent/:eventId',authenticate(),eventController.updateEvent);
 router.get('/searchEvents', eventController.searchEvents);
 router.get('/filterhEvents', eventController.filterhEvents);
 router.post('/joinEvent/:eventId', authenticate(), eventController.joinEvent);
 router.post('/leaveEvent/:eventId', authenticate(), eventController.leaveEvent);
 
 //commnets
-router.post(
-  '/addComment/:eventId',
-  authenticate(),
-  commentController.addComment
-);
-router.delete(
-  '/deleteComment/:commentId',
-  authenticate(),
-  commentController.deleteComment
-);
-router.put(
-  '/updateComment/:commentId',
-  authenticate(),
-  commentController.updateComment
-);
+router.post('/addComment/:eventId',authenticate(),commentController.addComment);
+router.delete('/deleteComment/:commentId', authenticate(),commentController.deleteComment);
+router.put('/updateComment/:commentId',authenticate(),commentController.updateComment);
 
 //likes
 router.post('/likeEvent/:eventId', authenticate(), likeController.likeEvent);
-router.delete(
-  '/removeLikeEvent/:eventId',
-  authenticate(),
-  likeController.removeLikeEvent
+router.delete('/removeLikeEvent/:eventId',authenticate(),likeController.removeLikeEvent
 );
 module.exports = router;
