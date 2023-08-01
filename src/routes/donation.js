ff55d7d601d5fb4d7f4a022cf780efd91807e948
Ilayda Nur Guzel <ilaydanurguzel1@gmail.com>
2023-08-01 13:50:10 +0300

    add donation functionality

---
+++ src/routes/donation.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const authenticate = require('../middleware/authenticate');
+const router = express.Router();
+
+const donationController = require('../controllers/donationController');
+
+router.post('/donations', authenticate(), donationController.createDonation);
+router.get('/donations', donationController.getAllDonations);
+router.get('/donations/:donationId', donationController.getDonationById);
+router.put('/donations/:donationId/status', authenticate(), donationController.updateDonationStatus);
+router.delete('/donations/:donationId', authenticate(), donationController.deleteDonation);
+router.get('/events/:eventId/totalDonations', authenticate(), donationController.getTotalDonationsForEvent);
+router.get('/donations/topDonors', donationController.getTopDonors);
+
+module.exports = router;
