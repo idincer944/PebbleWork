71d2e20b26188de6ec7074665502710e987d4b59
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-08-09 00:35:15 +0300

    add getAllBlogs function

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,20 +1,41 @@
 const Blog = require('../models/blog');
+const { validateBlog } = require('../utils/validations');
 // const { validateComment } = require('../utils/validations');
 
 module.exports = {
-
-    createNewBlog:async(req,res)=>{
-        const{title,content}=req.body;
-        try{ 
-        const author = req.user.user_id;
-        const Newblog=Blog({title,content,author});
-        const savedBlog=await Newblog.save();
-        res.status(201).json(savedBlog); // Success status code 201 - Created
-    } catch (error) {
+  createNewBlog : async(req,res) =>{
+  const validationResult = validateBlog(req.body);
+  if (validationResult.error) 
+  {
+      // Validation failed, handle the error with custom messages
+      const errorMessages = validationResult.error.details.map(
+        (error) => error.message);
+      return res.status(400).json({ errors: errorMessages });
+  }
+  const { title,content }= validationResult.value;
+  const author = req.user.user_id;
+  try{
+    const Newblog=Blog({title,content,author});
+      const savedBlog=await Newblog.save();
+      res.status(201).json(savedBlog); // Success status code 201 - Created
+    }
+    catch (error) { 
       console.error(error);
-      res.status(500).send('Internal Server Error while creating blog'); // Failure status code 500
+      res.status(500).send('Internal Server Error while creating blog');
+    } // Failure status code 500
+  },
+    
+  getAllBlogs : async(req,res) => {
+    try {
+      const blogs = await Blog.find({}).populate({
+          path: 'author',
+          select: 'title image'});
+      res.status(200).send(blogs);
+    } 
+    catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
     }
-        res.send("Hello Word");
-    }
+    },
 
 };
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -5,6 +5,7 @@ const blogController = require('../controllers/blogController');
 
 
 router.post('/create',authenticate, blogController.createNewBlog);
+router.get('/',authenticate, blogController.getAllBlogs);
 
 
 module.exports = router;
