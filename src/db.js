f884ce4416f9cc68f3637fad8d00adfbf0aa24e0
İsmail <idincer944@gmail.com>
2023-08-08 18:51:10 +0300

    changed db config

--- src/db.js
+++ src/db.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const url = process.env.DB_URL;
+const url = process.env.NODE_ENV === 'test'
+  ? process.env.DB_URL_TEST
+  : process.env.DB_URL;
 
 const connectToMongo = () => {
   mongoose.connect(url, { useNewUrlParser: true });
