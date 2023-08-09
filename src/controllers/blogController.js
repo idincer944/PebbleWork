397ed533b1f5c25ccb9e29a993ce40160e3aedd1
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-08-09 16:28:29 +0300

    add updatedBlog function

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -3,46 +3,45 @@ const { validateBlog } = require('../utils/validations');
 // const { validateComment } = require('../utils/validations');
 
 module.exports = {
-  createNewBlog : async(req,res) =>{
-  const validationResult = validateBlog(req.body);
-  if (validationResult.error) 
-  {
+  createNewBlog: async (req, res) => {
+    const validationResult = validateBlog(req.body);
+    if (validationResult.error) {
       // Validation failed, handle the error with custom messages
       const errorMessages = validationResult.error.details.map(
-        (error) => error.message);
+        (error) => error.message
+      );
       return res.status(400).json({ errors: errorMessages });
-  }
-  const { title,content }= validationResult.value;
-  const author = req.user.user_id;
-  try{
-    const Newblog=Blog({title,content,author});
-      const savedBlog=await Newblog.save();
-      res.status(201).json(savedBlog); // Success status code 201 - Created
     }
-    catch (error) { 
+    const { title, content } = validationResult.value;
+    const author = req.user.user_id;
+    try {
+      const Newblog = Blog({ title, content, author });
+      const savedBlog = await Newblog.save();
+      res.status(201).json(savedBlog); // Success status code 201 - Created
+    } catch (error) {
       console.error(error);
       res.status(500).send('Internal Server Error while creating blog');
     } // Failure status code 500
   },
-    
-  getAllBlogs : async(req,res) => {
+
+  getAllBlogs: async (req, res) => {
     try {
       const blogs = await Blog.find({}).populate({
-          path: 'author',
-          select: 'title image'});
+        path: 'author',
+        select: 'title image',
+      });
       res.status(200).send(blogs);
-    } 
-    catch (error) {
+    } catch (error) {
       console.error(error);
       res.status(500).send('Internal Server Error');
     }
-    },
-  getBlogById : async (req, res) => {
+  },
+  getBlogById: async (req, res) => {
     const { blogId } = req.params;
     try {
       const blog = await Blog.findById(blogId).populate({
         path: 'author',
-        select: 'title',
+        select: 'id',
       });
       if (!blog) {
         return res.status(404).json({ error: 'Blog not found' });
@@ -52,5 +51,25 @@ module.exports = {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   },
-
+  updatedBlog: async (req, res) => {
+    const { blogId } = req.params;
+    const blogData = req.body;
+    const userId = req.user.user_id;
+    try {
+      const blog = await Blog.findById(blogId);
+      console.log(blog);
+      if (!blog) {
+        return res.status(404).json({ error: 'Blog not found' });
+      }
+      // we are not allowing the user to change the other properties.
+      blog.title = blogData.title || blog.title;
+      blog.content = blogData.content || blog.content;
+      await blog.save();
+      res.status(200).json({ message: 'Blog updated successfully', blog });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: 'Internal Server Error while updating blog' });
+    }
+  },
 };
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -3,10 +3,9 @@ const { authenticate, isAdmin } = require('../middleware/authenticate');
 const router = express.Router();
 const blogController = require('../controllers/blogController');
 
-
-router.post('/create',authenticate, blogController.createNewBlog);
-router.get('/',authenticate, blogController.getAllBlogs);
+router.post('/create', authenticate, blogController.createNewBlog);
+router.get('/', authenticate, blogController.getAllBlogs);
 router.get('/:blogId', blogController.getBlogById);
-
+router.put('/:blogId', authenticate, blogController.updatedBlog);
 
 module.exports = router;
